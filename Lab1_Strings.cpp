e8f1a74b578a6b4bcd7238032d3ea34ee7c663b6
Anna5511 <segedianna2007@gmail.com>
2026-03-14 13:08:06 +0300

    Ура у нас теперь нормальный ввод из файла + маркер ставится в конец строки

--- Lab1_Strings.cpp
+++ Lab1_Strings.cpp
@@ -5,21 +5,16 @@
 const unsigned N = 100;
 struct strm{ char mark; char A[N + 1]; };
 
-void exit(std::string msg)
-{
-    std::cin >> "Ошибка выполнения: " << msg;
-    exit(-2);
-}
+
 
 bool inp(strm& a, char& stop) {
     std::ifstream file("C:\\Users\\Анечка\\Documents\\in.txt", std::ios::in);
+    std::ofstream out("C:\\Users\\Анечка\\Documents\\out.txt", std::ios::out);
+
     if (!file.is_open()) {
         std::cout << "Ошибка открытия входного файла";
         return false;
-    }
-
-    std::ofstream out("C:\\Users\\Анечка\\Documents\\out.txt", std::ios::out);
-    out.open("out.txt", std::ios::out);
+    }     
     if (!out.is_open()) {
         std::cout << "Ошибка открытия выходного файла";
         return false;
@@ -29,7 +24,6 @@ bool inp(strm& a, char& stop) {
         out << "Пустой файл";
         return false;
     }
-
     if (!(file >> stop)) {
         out << "Нет ограничителя";
         return false;
@@ -42,10 +36,14 @@ bool inp(strm& a, char& stop) {
         if (c == stop || c == '\n' || file.eof() || c == a.mark) break;
         a.A[i] = c;
         i++;
-        if (i >= N) break;
+        if (i >= N) {
+            std::cout << "В файле больше 100 символов";
+            break;
+        }
     }
 
     a.A[i] = a.mark;
+    a.A[i] = '\0';
     out.close();
     file.close();
     return true;
@@ -58,7 +56,10 @@ int main(){
 	setlocale(LC_ALL, "RU");
 
     strm file;
-    char mark;
+    char stop;
+
+    inp(file, stop);
+    std::cout << std::endl << file.mark << std::endl << file.A;
 
 	return 0;
 }
