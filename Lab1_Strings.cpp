93523e952a12e3ea9cef0a95e7f54b92d828409d
Anna5511 <segedianna2007@gmail.com>
2026-03-15 16:29:43 +0300

    ну просто ужас(

--- Lab1_Strings.cpp
+++ Lab1_Strings.cpp
@@ -14,84 +14,11 @@ struct strm{ char mark; char A[N + 1]; };
 void outp(const char* text, const char* str, char c)
 {
     std::ofstream file("C:\\Users\\Анечка\\Documents\\out.txt", std::ios::app);
-    file << text << " : " << c << str << std::endl;
+    file << text << c << str << std::endl;
 
     file.close();
 }
 
-/// <summary>
-/// Функция ввода из файла
-/// </summary>
-/// <param name="a"> - структура {маркер ; строка с маркером} </param>
-/// <returns></returns>
-bool inp(strm& a) {
-    int file_num = 1;
-    std::ifstream file("C:\\Users\\Анечка\\Documents\\in.txt", std::ios::in);
-    std::ofstream out("C:\\Users\\Анечка\\Documents\\out.txt", std::ios::out);
-
-    char stop;
-    char c;
-
-    if (!file.is_open()) {
-        std::cout << "Ошибка открытия входного файла";
-        return false;
-    }     
-    if (!out.is_open()) {
-        std::cout << "Ошибка открытия выходного файла";
-        return false;
-    }
-
-    outp("Cтрока № :", "", file_num);
-
-
-    if (!(file >> a.mark)) {
-        std::cout << "Ошибка - пустой файл";
-        outp("Ошибка - пустой файл", "", ' ');
-        return false;
-    }
-    if (!(file >> stop)) {
-        std::cout << "Ошибка - нет ограничителя";
-        outp("Ошибка - нет ограничителя", "", ' ');
-        return false;
-    }
-
-    unsigned i = 0;
-    
-
-    while (file.get(c)) {
-        if (c == stop || c == '\n' || file.eof() || c == a.mark) break;
-        a.A[i] = c;
-        i++;
-        if (i >= N) {
-            outp("Ошибка - в файле больше 100 символов", "", ' ');
-            std::cout << "Ошибка - в файле больше 100 символов";
-            break;
-        }
-    }
-        
-    if (i == 0) {
-        outp("Ошибка - пустая строка", "", ' ');
-        std::cout << "Ошибка - пустая строка";
-        return 0;
-    }
-
-    if (i == N) {
-        outp("В файле ровно 100 символов", "", ' ');
-        std::cout << "В файле ровно 100 символов";
-    }
-
-    a.A[i] = a.mark;
-    a.A[i + 1] = '\0';
-    
-    outp("Маркер", "", a.mark );
-    outp("Ограничитель", "", stop);
-    outp("Строка", a.A, ' ');
-
-    
-    out.close();
-    file.close();
-    return true;
-}
 
 /// <summary>
 /// Перемещает символы в строке
@@ -138,8 +65,101 @@ void process(strm& a, int o_pos, int n_pos)
         o_pos--;
     }
 
-    outp("Результат", a.A, ' ');
+    outp("Результат : ", a.A, ' ');
 
+}
+
+
+/// <summary>
+/// Функция ввода из файла
+/// </summary>
+/// <param name="a"> - структура {маркер ; строка с маркером} </param>
+/// <returns></returns>
+bool inp(strm& a) {
+    int file_num = 1;
+    std::ifstream file("C:\\Users\\Анечка\\Documents\\in.txt", std::ios::in);
+    std::ofstream out("C:\\Users\\Анечка\\Documents\\out.txt", std::ios::out);
+
+    char stop;
+    char c;
+
+    if (!file.is_open()) {
+        std::cout << "Ошибка открытия входного файла";
+        return false;
+    }
+    if (!out.is_open()) {
+        std::cout << "Ошибка открытия выходного файла";
+        return false;
+    }
+
+
+
+    while (true) {
+        char n = file_num + '0';
+        outp("Cтрока №", "", n);
+
+
+        file >> a.mark;
+        if (a.mark == '\n') {
+            outp("Ошибка - пустая строка", "", ' ');
+            
+            return false;
+        }
+
+        file >> stop;
+        if (stop == '\n') {
+            outp("Ошибка - нет ограничителя", "", ' ');
+            return false;
+        }
+
+        unsigned i = 0;
+
+        while (file.get(c)) {
+            if (c == stop || c == '\n' || file.eof() || c == a.mark) break;
+            a.A[i] = c;
+            i++;
+            if (i >= N) {
+                outp("Ошибка - в строке больше 100 символов", "", ' ');
+                break;
+            }
+        }
+
+        if (i == 0) {
+            outp("Ошибка - пустая строка", "", ' ');
+            return 0;
+        }
+
+        if (i == N) {
+            outp("В строке ровно 100 символов", "", ' ');
+            std::cout << "В строке ровно 100 символов";
+        }
+
+        a.A[i] = a.mark;
+        a.A[i + 1] = '\0';
+
+        outp("Маркер: ", "", a.mark);
+        outp("Ограничитель: ", "", stop);
+        outp("Строка: ", a.A, ' ');
+
+        while (file.get(c)) {
+            if (c == '\n') break;
+            if (file.eof()) return true;
+        }
+
+        file_num++;
+        
+
+        int o_pos = poisk_pos(a);
+        int n_pos = (poisk_pos(a)) / 2 + (poisk_pos(a)) % 2;
+        process(a, o_pos, n_pos);
+
+        outp("----------------", "", ' ');
+
+
+    }
+    out.close();
+    file.close();
+    return true;
 }
 
 int main(){
@@ -148,9 +168,7 @@ int main(){
 
     if(!(inp(file))) return 0;
 
-    int o_pos = poisk_pos(file);
-    int n_pos = (poisk_pos(file))/2+(poisk_pos(file))%2;
-    process(file, o_pos, n_pos);
+    
 
 	return 0;
 }
