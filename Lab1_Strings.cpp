6a6799c51fde7713002328a8ab0c8204e22bf545
Anna5511 <segedianna2007@gmail.com>
2026-03-15 14:58:03 +0300

    Ура первая версия программы готова Тут структуры + перестановка символов в строке (swap)

--- Lab1_Strings.cpp
+++ Lab1_Strings.cpp
@@ -5,11 +5,17 @@
 const unsigned N = 100;
 struct strm{ char mark; char A[N + 1]; };
 
-
-
+/// <summary>
+/// Функция ввода из файла
+/// </summary>
+/// <param name="a"> - структура {маркер ; строка с маркером} </param>
+/// <param name="stop"> - маркер </param>
+/// <returns></returns>
 bool inp(strm& a, char& stop) {
     std::ifstream file("C:\\Users\\Анечка\\Documents\\in.txt", std::ios::in);
     std::ofstream out("C:\\Users\\Анечка\\Documents\\out.txt", std::ios::out);
+
+    char c;
 
     if (!file.is_open()) {
         std::cout << "Ошибка открытия входного файла";
@@ -24,13 +30,19 @@ bool inp(strm& a, char& stop) {
         out << "Пустой файл";
         return false;
     }
+    file >> c;
+    if (c != '\n') file.seekg(-1, std::ios::cur);
+
     if (!(file >> stop)) {
         out << "Нет ограничителя";
         return false;
     }
 
+    file >> c;
+    if (c != '\n') file.seekg(-1, std::ios::cur);
+
     unsigned i = 0;
-    char c;
+    
 
     while (file.get(c)) {
         if (c == stop || c == '\n' || file.eof() || c == a.mark) break;
@@ -43,23 +55,72 @@ bool inp(strm& a, char& stop) {
     }
 
     a.A[i] = a.mark;
-    a.A[i] = '\0';
+    a.A[i+1] = '\0';
     out.close();
     file.close();
     return true;
 }
 
+/// <summary>
+/// Перемещает символы в строке
+/// </summary>
+/// <param name="a"> - структура {маркер ; строка с маркером} </param>
+/// <param name="len"> - изначальная позиция маркера </param>
+void swap(strm& a, int len)
+{
+    char temp = a.A[0];
+    for (int i = 0; i < len; i++) {
+        a.A[i] = a.A[i+1];
+    }
+    a.A[len] = temp;
+}
 
+/// <summary>
+/// Ищет изначальную позицию маркера
+/// </summary>
+/// <param name="a"> - структура {маркер ; строка с маркером} </param>
+/// <returns></returns>
+int poisk_pos(strm& a) {
+    int o_pos = 0;
 
+    while (a.A[o_pos] != '\0')
+    {
+        if (a.A[o_pos] == a.mark) {
+            return o_pos;
+        }
+        o_pos++;
+    }
+}
+
+/// <summary>
+/// Основная работа программы - перемещение символов в строке
+/// </summary>
+/// <param name="a"> - структура {маркер ; строка с маркером} </param>
+/// <param name="o_pos"> - old marker position (конец строки)</param>
+/// <param name="n_pos"> - new marker position (середина строки)</param>
+void process(strm& a, int o_pos, int n_pos)
+{
+    int len = o_pos;
+    while (o_pos != n_pos) {
+        swap(a, len);
+        o_pos--;
+    }
+
+}
 
 int main(){
-	setlocale(LC_ALL, "RU");
+	setlocale(LC_ALL, "ru");
 
     strm file;
     char stop;
 
     inp(file, stop);
-    std::cout << std::endl << file.mark << std::endl << file.A;
+    std::cout << file.mark << std::endl << file.A;
+    int o_pos = poisk_pos(file);
+    int n_pos = (poisk_pos(file))/2+(poisk_pos(file))%2;
+    process(file, o_pos, n_pos);
 
+    std::cout << std::endl;
+    std::cout << file.A;
 	return 0;
 }
